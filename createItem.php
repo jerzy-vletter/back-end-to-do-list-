85d0c8b46ed0bebe5af811b06b1d9baa30569e16
J. vletter <99052114@mydavinci.nl>
2021-11-02 13:12:25 +0100

    (works) posting data into db from the create form.

--- createItem.php
+++ createItem.php
@@ -1,16 +1,34 @@
+<?php
+
+require "connection.php";
+require "mainUIPage.php";
+require "mainEngine.php";
+
+# checks if the corresponding form on the page uses POST to send data, header in just a redirect.
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $listname = $_POST['listName'];
+    createList($conn, $listname);
+
+    header("location: index.php");
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <h1>CREATE YOUR ITEMS</h1>
     </head>
     <body>
-        <form action="mainEngine.php" method="post">
+        <form method="post">
         
             Name:  <input type="text" name="listName" placeholder="listname"><br />
             <input type="submit" name="submit" value="create item" />
         </form>
+
     </body>
 </html>
 <?php 
+
 
 ?>
--- mainEngine.php
+++ mainEngine.php
@@ -5,12 +5,15 @@
 
 
 #inserting data from the add item form into the database
-if(isset($_POST['submit'])){
-    $listname = $_POST['listName']; 
 
-    $pdoQuery = "INSERT INTO name('name') VALUES (:listname)";
-    $pdoQuery_run = $conn->prepare($pdoQuery);
-    $pdoQuery_exec = $pdoQuery_run->execute(array(':name'=>$listname));
+
+function createList($conn, $listname){
+
+    #insert using data created and made into variables on the createItem.php page.
+        $pdoQuery = "INSERT INTO list(name) VALUES (:listName)"; 
+        $pdoQuery_run = $conn->prepare($pdoQuery);
+        $pdoQuery_run->bindParam(':listName', $listname);
+        $pdoQuery_run->execute();
+
 }
-
 ?>
